32171944590eb2cd94346ac1fe444453effe5fb6
wuwenjie03 <wuwenjie03@meituan.com>
2016-12-25 21:05:35 +0800

    add simple hash

--- src/custom_preprocess.py
+++ src/custom_preprocess.py
@@ -18,9 +18,30 @@ def load_raw_data_and_split_by_dt(path, output_dir):
             output_file_dict[dt].write(",".join(row[:2] + [hour] + row[3:]) + "\n")
 
 
+def load_raw_data_and_hash_and_split_by_dt(path, output_dir):
+    base_datetime = datetime.strptime('141021', '%y%m%d')
+    output_file_dict = {(base_datetime + timedelta(days=x)).strftime('%y%m%d'): open(
+        output_dir + '/' + (base_datetime + timedelta(days=x)).strftime('%y%m%d') + "_hashed" + '.csv', 'w') for x in range(0, 10)}
+
+    with open(path, 'rb') as csvfile:
+        header = csvfile.readline()
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            hour_column = row[2]
+            dt = hour_column[:6]
+            hour = hour_column[6:]
+            row[2] = hour
+            for i in range(2, 24):
+                row[i] = str(hash(row[i]) % 1024)
+            output_file_dict[dt].write(",".join(row[1:]) + "\n")
+
+
 class TestCustomPreprocess(unittest.TestCase):
     def test_load_raw_data_and_split_by_dt(self):
         load_raw_data_and_split_by_dt('../fixtures/train.thumb', '../fixtures')
+
+    def test_load_raw_data_and_hash_and_split_by_dt(self):
+        load_raw_data_and_hash_and_split_by_dt('../fixtures/train.thumb', '../fixtures')
 
 
 if __name__ == '__main__':
